f169af78bafed20cb37ba57b78abd1e5a33d28fe
NorSP <norbissilvera@gmail.com>
2022-09-29 20:45:58 +0000

    comentario

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomPalos = Math.floor(Math.random() * palos.length);
+  let randomNumeros = Math.floor(Math.random() * numeros.length);
+
+  let returnPaloI = (document.getElementById("palo").innerHTML =
+    palos[randomPalos]);
+  let returnNumero = (document.getElementById("numero").innerHTML =
+    numeros[randomNumeros]);
+  let returnPaloF = (document.getElementById("palo2").innerHTML =
+    palos[randomPalos]);
+
+  return {
+    returnPaloI,
+    returnNumero,
+    returnPaloF
+  };
 };
